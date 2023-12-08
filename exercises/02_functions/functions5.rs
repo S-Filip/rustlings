8a032087bc5fc53c8b7fd85cd923c0b0f2c8a03c
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-08 13:44:08 +0100

    Functions solutions

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,13 +3,20 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let answer = square(3);
-    println!("The square of 3 is {}", answer);
+    let answer1 = square1(3);
+    println!("The square of 3 is {}", answer1);
+    let answer2 = square2(5);
+    println!("the square of 5 is {}", answer2);
 }
 
-fn square(num: i32) -> i32 {
-    num * num;
+fn square1(num: i32) -> i32 {
+    let sol = num * num;
+    return sol; 
+}
+
+// Or
+
+fn square2(num: i32) -> i32 {
+    num * num
 }
